967dbf59348694bc492a2f0490bd0f9862523e96
Justin Lin <justinwlin@gmail.com>
2021-02-05 11:09:27 -0800

    Update buttonGame.js

--- OneButtonGame/buttonGame.js
+++ OneButtonGame/buttonGame.js
@@ -7,10 +7,11 @@ function setup() {
   g = 0;
   b = 0;
   
-  centerx = 360
-  centery = 200
-  indexText = 0
-  words=["Don't click", "stop!", "urgh", "why", "are", "you", "so", "annoying", "you must have something wrong in your head?", "i'll draw you a square?", "what about a circle?", "what about if i make them move?", "ok I give up", "you get nothing"]
+  var centerx = 360
+  var centery = 200
+  var indexText = 0
+  var radius = 200
+  var words=["Don't click", "stop!", "urgh", "why", "are", "you", "so", "annoying", "you must have something wrong in your head?", "i'll draw you a square?", "what about a circle?", "what about if i make them move?", "ok I give up", "you get nothing"]
 }
 
 function draw() {
@@ -19,11 +20,12 @@ function draw() {
   strokeWeight(2);
   stroke(r, g, b);
   fill(r, g, b, 127);
-  ellipse(centerx, centery, 200, 200);
+  ellipse(centerx, centery, radius, radius);
   fill(255, 255, 255)
 
   textSize(30)
   text(words[indexText], centerx-300, centery+150);
+  
   wordText = words[indexText]
   if(indexText == 0){
     
